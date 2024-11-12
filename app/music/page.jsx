1024fd0cb591b4917019abe1dac085e5b7d57b94
tbrown034 <tbrown034@gmail.com>
2024-11-12 04:23:17 -0500

    chore: trying to fix deployment issue

--- app/music/page.jsx
+++ app/music/page.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const MusicPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
--- app/tv/page.jsx
+++ app/tv/page.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const TVPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
